4a904a2a255ea5c4bb69bfc74e3a24ab7a7d0f3b
Edson Shideki Kokado <eskokado@gmail.com>
2018-02-28 16:06:48 -0300

    Cadastrando usuário - corrreção

--- src/providers/user/user.service.ts
+++ src/providers/user/user.service.ts
@@ -3,12 +3,16 @@ import { Injectable } from '@angular/core';
 import { AngularFireModule, FirebaseApp } from 'angularfire2';
 import { AngularFireDatabase } from 'angularfire2/database';
 import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/first';
 import { UserModel } from '../../models/user.model';
 import { HttpClientModule } from '@angular/common/http';
 import { firestore } from 'firebase/app';
+import { FirebaseOperation, AngularFireList } from 'angularfire2/database/interfaces';
 
 @Injectable()
 export class UserService {
+
+  users: AngularFireList<UserModel[]>;
 
   constructor(
     public afd: AngularFireDatabase,
@@ -17,9 +21,9 @@ export class UserService {
     
   }
 
-  create(user: UserModel) :Promise<void> {
-    return this.afd.object(`/users`)
-      .set(user);
+  create(user: UserModel) {
+    return this.afd.list(`/users`)
+      .push(user);
   }
 
 }
