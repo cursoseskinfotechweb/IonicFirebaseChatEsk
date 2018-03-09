092c3db00177fe2a5b381319abfd3fdbb82b5c8d
Edson Shideki Kokado <eskokado@gmail.com>
2018-03-09 13:57:54 -0300

    Lista de chats no ChatService

--- src/providers/chat/chat.service.ts
+++ src/providers/chat/chat.service.ts
@@ -3,18 +3,38 @@ import { Injectable } from '@angular/core';
 import { BaseService } from '../base.service';
 import { Chat } from '../../models/chat.model';
 import { AngularFireAuth } from 'angularfire2/auth';
-import { AngularFireDatabase, AngularFireObject } from 'angularfire2/database';
+import { AngularFireDatabase, AngularFireObject, AngularFireList } from 'angularfire2/database';
+
+import * as firebase from 'firebase/app';
 
 @Injectable()
 export class ChatService extends BaseService {
 
+  chats: AngularFireList<Chat>;
+  
   constructor(
     public afAuth: AngularFireAuth,
     public db: AngularFireDatabase,
     public http: HttpClient
   ) {
     super();
+    this.setChats();
   }
+
+  private setChats(): void {
+    this.afAuth.authState
+      .subscribe((authUser: firebase.User) => {
+        if (authUser) {
+
+          this.chats = this.db.list<Chat>(`/chats/${authUser.uid}`, 
+            (ref: firebase.database.Reference) => ref.orderByChild('timestamp')
+          );
+          
+          console.log('Lista de chats: ', this.chats)
+        }
+      }
+    );
+   }
 
   create(chat: Chat, userId1: string, userId2: string): Promise<void> {
     return this.db.object<Chat>(`/chats/${userId1}/${userId2}`)
