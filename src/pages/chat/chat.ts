003b080c5d80785572f9ad7954c5bc1adb0d07b7
Edson Shideki Kokado <eskokado@gmail.com>
2018-03-09 16:21:52 -0300

    Lista de mensagens da ChatPage

--- src/pages/chat/chat.ts
+++ src/pages/chat/chat.ts
@@ -1,8 +1,12 @@
-import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { Component, ViewChild } from '@angular/core';
+import { IonicPage, NavController, NavParams, Content } from 'ionic-angular';
 import { AuthService } from '../../providers/auth/auth.service';
 import { User } from '../../models/user.model';
 import { UserService } from '../../providers/user/user.service';
+import { AngularFireList } from 'angularfire2/database';
+import { Message } from '../../models/message.model';
+import { MessageService } from '../../providers/message/message.service';
+import { Observable } from 'rxjs/Observable';
 
 @Component({
   selector: 'page-chat',
@@ -10,15 +14,18 @@ import { UserService } from '../../providers/user/user.service';
 })
 export class ChatPage {
 
-  messages: string[] = [];
+  @ViewChild(Content) content: Content;
+  messages: AngularFireList<Message>;
+  viewMessages: Observable<Message[]>;
   pageTitle: string;
   sender: User;
   recipient: User;
 
   constructor(
+    public authService: AuthService,
+    public messageService: MessageService,
     public navCtrl: NavController, 
     public navParams: NavParams,
-    public authService: AuthService,
     public userService: UserService
   ) {
   }
@@ -38,12 +45,54 @@ export class ChatPage {
       .subscribe((currentUser: User) => {
         this.sender = currentUser;
         console.log(' ionViewDidLoad sender', this.sender)
-      })
+
+        let doSubscription = () => {
+          this.viewMessages = this.messageService.mapListKeys<Message>(this.messages);
+          this.viewMessages
+            .subscribe((messages: Message[]) => {
+              this.scrollToBottom();
+            }
+          );
+        };
+
+        this.messages = this.messageService
+          .getMessages(this.sender.$key, this.recipient.$key);
+
+          this.messages
+          .valueChanges()
+          .first()
+          .subscribe((messages: Message[]) => {
+
+            if (messages.length === 0) {
+
+              this.messages = this.messageService
+                .getMessages(this.recipient.$key, this.sender.$key);
+
+              doSubscription();
+
+            } else {
+              doSubscription();
+            }
+
+          }
+        );
+
+      }
+    );
 
   }
 
 
   sendMessage(newMessage: string) :void {
-    this.messages.push(newMessage);
+    //this.messages.push(newMessage);
   }
+
+  private scrollToBottom(duration?: number): void {
+    setTimeout(() => {
+      if (this.content._scroll) {
+        this.content.scrollToBottom(duration || 300);
+      }
+    }, 50);
+  }
+
 }
