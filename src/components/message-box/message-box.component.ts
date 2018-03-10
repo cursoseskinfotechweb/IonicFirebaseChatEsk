35ffc643d8aaaec7f10f26be0ff9df28b9c3643f
Edson Shideki Kokado <eskokado@gmail.com>
2018-03-10 14:31:33 -0300

    Component para exibir mensagem (parte 2)

--- src/components/message-box/message-box.component.ts
+++ src/components/message-box/message-box.component.ts
@@ -3,7 +3,11 @@ import { Message } from '../../models/message.model';
 
 @Component({
   selector: 'message-box',
-  templateUrl: 'message-box.component.html'
+  templateUrl: 'message-box.component.html',
+  host: {
+    '[style.justify-content]': '((!isFromSender) ? "flex-start" : "flex-end")',
+    '[style.text-align]': '((!isFromSender) ? "left" : "right")'
+  }
 })
 export class MessageBoxComponent {
 
