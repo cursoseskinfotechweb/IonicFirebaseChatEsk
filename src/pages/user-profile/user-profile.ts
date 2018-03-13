44769b9564453565d35805084eb4846b60022bac
Edson Shideki Kokado <eskokado@gmail.com>
2018-03-13 13:20:45 -0300

    Editando informações do usuário

--- src/pages/user-profile/user-profile.ts
+++ src/pages/user-profile/user-profile.ts
@@ -34,4 +34,22 @@ export class UserProfilePage {
     );
   }
 
+  onSubmit(event: Event): void {
+    event.preventDefault();
+    
+    this.editUser();
+  }
+
+  private editUser(photoUrl?: string): void {
+    this.userService
+      .edit({
+        name: this.currentUser.name,
+        username: this.currentUser.username,
+        photo: photoUrl || this.currentUser.photo || ''
+      }).then(() => {
+        this.canEdit = false;
+      });
+  }
+
+
 }
